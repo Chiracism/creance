ae645a74117278f0a62af8b26e0143ff827676d0
Chirac Ilunga <tshitengailunga@icloud.com>
2022-07-01 14:45:02 +0100

    static

--- Django-Bank/src/bankingsystem/settings.py
+++ Django-Bank/src/bankingsystem/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 django_heroku.settings(locals())
 MEDIA_URL = "/media/"
